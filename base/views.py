bde2ba6dfc771e7d4de1a56697eec1065bb3a51b
Akshith2307 <mgakshith@gmail.com>
2024-03-11 21:01:40 -0700

    Read data from database

--- base/views.py
+++ base/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from .models import Product
 from .p import products
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializer  import ProductSerializer 
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -13,14 +14,13 @@ def getRoutes(request):
     return Response(routes)
 @api_view(['GET'])
 def getProducts (request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProduct (request, pk):
-    Product=None
-    for i in products:
-        if i["_id"] == pk:
-            product = i
-            break
-    return Response(product)
+    products = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(products, many=False)
+    return Response(serializer.data)
 
